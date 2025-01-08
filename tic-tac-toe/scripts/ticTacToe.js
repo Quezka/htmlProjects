201a3d9fe3464e8b08d11e5b4d0fe413e04a49e6
Quezka <arsdom15@outlook.com>
2025-01-09 00:15:22 +0100

    finished tic tac toe website

--- tic-tac-toe/scripts/ticTacToe.js
+++ tic-tac-toe/scripts/ticTacToe.js
@@ -1,48 +1,60 @@
 const xchar = "X";
 const ochar = "O";
-let giocatore = xchar;
-let schema = [ '', '', '', '', '', '', '', '', ''];
+let player = xchar;
+let scheme = [ '', '', '', '', '', '', '', '', ''];
 
-function mossa(x) {
-    if (schema[x] === '') {
-        schema[x] = giocatore;
+function move(x) {
+    if (scheme[x] === '') {
+        scheme[x] = player;
         let cell = document.getElementsByTagName('td')[x];
-        cell.innerHTML = giocatore;
-        if (haiVinto()) {
-            alert(cell.innerHTML + ' ha vinto');
-            resetGioco();
-        } else if (pareggio()) {
-            alert('Pareggio');
-            resetGioco();
+        cell.innerHTML = player;
+        if (win()) {
+            alert(cell.innerHTML + ' has won!');
+            resetGame();
+        } else if (tie()) {
+            alert('Tie!');
+            resetGame();
         } else {
-            giocatore = giocatore === xchar ? ochar : xchar;
+            player = player === xchar ? ochar : xchar;
         }
     }
 }
 
-function haiVinto() {
-    /*    if () {
+function win() {
+    const combinazioniVittoria = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+      ];
+    for (let combinazione of combinazioniVittoria) {
+        const [a, b, c] = combinazione;
+        if (scheme[a] && scheme[a] === scheme[b] && scheme[a] === scheme[c]) {
             return true;
         }
-
+    }
     return false;
-    */
 }
-function pareggio() {
-    for (let i = 0; i < 9; i++) {
-        if (schema[i] === '') {
+function tie() {
+    let cells = document.getElementsByTagName('td');
+    for (let i = 0; i < cells.length; i++) {
+        if (scheme[i] === '') {
             return false;
         }
     }
     return true;
 }
 
-function resetGioco() {
-    schema = [ '', '', '', '', '', '', '', '', ''];
+function resetGame() {
+    scheme = [ '', '', '', '', '', '', '', '', ''];
 
-    giocatore = xchar;
+    player = xchar;
     let cells = document.getElementsByTagName('td');
     for (let i = 0; i < cells.length; i++) {
-
+        cells[i].innerHTML = "";
     }
 }
